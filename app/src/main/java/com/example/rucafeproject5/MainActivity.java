a6bb705a6bd82c4f9a3efe8c85b91374d51663ca
Manveer Singh <66374399+Mgrewal2000@users.noreply.github.com>
2021-04-12 23:10:49 -0400

    creation

---
+++ app/src/main/java/com/example/rucafeproject5/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.rucafeproject5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ *
+ */
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     *
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
